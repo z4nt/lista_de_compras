c7f4c80aff4a3990a2172e175211880bfd043d88
Antoniel Ferreira Sousa <Antoniel.Sousa@digitalcollege.internal>
2025-01-16 17:48:04 -0300

    ajeitei a logica de validação e de atualização

--- front-end/src/app/listagem/listagem.jsx
+++ front-end/src/app/listagem/listagem.jsx
@@ -2,9 +2,9 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Listagem({ order, refresh }) {
-    const [atualizar, setAtualizar] = useState(false);
+    const [atualizar, setAtualizar] = useState(null);
     const [produtos, setProdutos] = useState([]);
-    const [formData, setFormData] = useState({ nome: '', descricao: '', valor: '', disponivel: 'Sim' });
+
 
     useEffect(() => {
         const fetchProdutos = async () => {
@@ -44,17 +44,14 @@ export default function Listagem({ order, refresh }) {
     };
 
     const handleUpdate = async (id) => {
-        if (formData.nome === '' || formData.descricao === '' || formData.valor === '') {
-            alert('Preencha todos os campos');
-            return;
-        }
+        let produto = produtos.find(produto => produto.id === id);
         try {
             const response = await fetch(`http://localhost:3001/api/produtos/${id}`, {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(formData)
+                body: JSON.stringify(produto)
             });
             if (response.ok) {
                 console.log('Produto atualizado com sucesso');
@@ -80,11 +77,11 @@ export default function Listagem({ order, refresh }) {
                     : item
             )
         );
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
+        // const { name, value } = e.target;
+        // setFormData({
+        //     ...formData,
+        //     [name]: value
+        // });
     };
 
     return (
@@ -92,7 +89,7 @@ export default function Listagem({ order, refresh }) {
             <ul>
                 {produtos.map((produto) => (
                     <li className='flex justify-between bg-gray-100 py-5 px-2 border-2 rounded-md my-2 border-grey-500' key={produto.id}>
-                        {atualizar ? (
+                        {atualizar === produto.id ? (
                             <>
                                 <input className='font-bold w-20' type='text' name='nome' value={produto.nome} onChange={(e) =>
                                     updateItem(e, produto.id, "nome", e.target.value)} />
@@ -104,7 +101,7 @@ export default function Listagem({ order, refresh }) {
                                     <option value='Sim'>Sim</option>
                                     <option value='Nao'>Nao</option>
                                 </select>
-                                <button className='bg-gray-100 text-white px-3 py-1 rounded' onClick={AtualizarTrue}>
+                                <button className='bg-gray-100 text-white px-3 py-1 rounded' onClick={() => setAtualizar(null)}>
                                     <img className='h-5 bg-red-400 p-1 rounded-md' src="/close.svg" alt="" />
                                 </button>
                                 <button className='bg-gray-100 text-white px-3 py-1 rounded' onClick={() => handleUpdate(produto.id)}>
@@ -119,7 +116,7 @@ export default function Listagem({ order, refresh }) {
                                 <p className='font-bold w-10'>R${produto.valor}</p>
                                 <p className='font-bold w-10'>{produto.disponivel ? 'Sim' : 'Nao'}</p>
                                 <div className='flex w-28 gap-2'>
-                                    <button className='bg-gray-100 text-white px-3 py-1 rounded' onClick={AtualizarTrue}>
+                                    <button className='bg-gray-100 text-white px-3 py-1 rounded' onClick={() => setAtualizar(produto.id)}>
                                         <img className='h-5 bg-gray-100' src="/edit.svg" alt="" />
                                     </button>
                                     <button className='bg-gray-100 text-white px-3 py-1 rounded' onClick={() => handleDelete(produto.id)}>
